ebf936210baaa4bf4009b3ede596bfcab9f3852a
David Shishkin <shishkindavid007@gmail.com>
2026-06-22 12:27:35 +0400

    2167A

---
+++ Tasks 800/2167A - Square.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Это шуточная задача квадрат можно составить только если все стороны равны
+    int t = []() {int x;cin >> x;return x;}();
+    while (t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << ((min({a,b,c,d}) == max({ a,b,c,d })) ? "YES" : "NO") << endl;
+    }
+}
+
+//2167A *800
