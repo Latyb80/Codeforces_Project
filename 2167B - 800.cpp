50c4244fca3f0217d5c0194709b60dec4205c817
David Shishkin <shishkindavid007@gmail.com>
2026-06-22 10:46:06 +0400

    2167B

---
+++ 2167B - 800.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;cin >> n;
+        string t, s;
+        cin >> t >> s;
+        sort(t.begin(), t.end());sort(s.begin(), s.end());
+        cout << ((t == s) ? "YES" : "NO") << endl;
+
+    }
+}
+
