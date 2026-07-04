1377485f27c0d64a6463a4c45105a0afe4ed92c5
David Shishkin <shishkindavid007@gmail.com>
2026-07-04 13:40:21 +0400

    236A

---
+++ Tasks 800/236A - Девушка или Юноша.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    // Здесь нам нужно просто убрать дубликаты
+    // и проверить чётность оставшейся длины
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());// Сортируем ведь unique видит только рядом стоящие дубликаты
+    s.erase(unique(s.begin(), s.end()), s.end()); // Unique не удаляет дубликаты он просто  
+    cout << ((s.length() % 2 == 0) ? "CHAT WITH HER!\n" : "IGNORE HIM!\n");// отправялет их в конец и возращает индекс начала дубликатов 
+}
+
+
+//236A *800
+
