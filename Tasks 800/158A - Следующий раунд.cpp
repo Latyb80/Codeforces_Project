e5d3de729e5838f41f257dc07dd510ee2ced2dc8
David Shishkin <shishkindavid007@gmail.com>
2026-07-10 14:51:49 +0400

    158A

---
+++ Tasks 800/158A - Следующий раунд.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Задача очень простая просто находим это число k в массиве 
+    // и сравниваем только есть один нюанс ещё надо проверить чтобы число было больше 0
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ab = a[k - 1];
+    cout << [&a, n, ab]() {int coun = 0; for (int i = 0; i < n; i++)if (a[i] >= ab && a[i] > 0)coun++; return coun; }() << endl;
+}
+
+
+//158A *800
+
