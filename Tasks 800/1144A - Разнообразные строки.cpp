6d8bc4010b99f1526b98dce61e17a473ef051b7a
David Shishkin <shishkindavid007@gmail.com>
2026-07-11 13:53:12 +0400

    1144A

---
+++ Tasks 800/1144A - Разнообразные строки.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--) {
+        string s;
+        cin >> s;
+        bool accepted = true;
+        sort(s.begin(), s.end());
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i - 1] + 1 != s[i]) {
+                accepted = false;
+            }
+        }
+        cout << ((accepted) ? "Yes\n" : "No\n") << endl;
+    }
+}
+
+
+//1144A *800
